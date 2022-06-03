05108b6bc185a51c9af8ca2cbb9d254e151ae77b
Gonzalo Martínez <ggmartinez.utu@gmail.com>
2022-06-03 14:40:45 -0300

    Modelo de Personita implementando Alta Creado proyecto de Capa Logica, y agregada referencia

--- CapaDeDatos/PersonitaModelo.cs
+++ CapaDeDatos/PersonitaModelo.cs
@@ -9,7 +9,42 @@ namespace CapaDeDatos
 {
     class PersonitaModelo : Modelo
     {
-       
+        public int Id;
+        public string Nombre;
+        public string Apellido;
+        public int Telefono;
+        public string Email;
 
+        public void Guardar()
+        {
+            if (this.Id.ToString() == "") GuardarSinId();
+            else GuardarConId();
+        }
+
+        private void GuardarConId()
+        {
+            comando.CommandText = "INSERT INTO personita VALUES (@id, @nombre,@apellido,@email,@telefono)";
+            comando.Parameters.AddWithValue("@id", this.Id.ToString());
+            comando.Parameters.AddWithValue("@nombre", this.Nombre);
+            comando.Parameters.AddWithValue("@apellido", this.Apellido);
+            comando.Parameters.AddWithValue("@telefono", this.Telefono.ToString());
+            comando.Parameters.AddWithValue("@email", this.Email);
+            comando.Prepare();
+            comando.ExecuteNonQuery();
+        }
+
+        private void GuardarSinId()
+        {
+            comando.CommandText = "INSERT INTO " +
+                            "personita (nombre, apellido, telefono, email) " +
+                            "VALUES (@nombre,@apellido,@telefono,@email)";
+
+            comando.Parameters.AddWithValue("@nombre", this.Nombre);
+            comando.Parameters.AddWithValue("@apellido", this.Apellido);
+            comando.Parameters.AddWithValue("@telefono", this.Telefono.ToString());
+            comando.Parameters.AddWithValue("@email", this.Email);
+            comando.Prepare();
+            comando.ExecuteNonQuery();
+        }
     }
 }
